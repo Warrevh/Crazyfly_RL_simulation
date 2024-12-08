0081783b425861b590a6e6a44e888549839856e0
Warrevh <warrevh10@gmail.com>
2024-12-08 23:16:32 +0100

    small changes

--- code/train.py
+++ code/train.py
@@ -30,14 +30,14 @@ parameters = {
     'ang_v': True, #!!!!!!! ADJUST MANUALY IN CODE !!!!!!!
     'prev_act':False, #!!!!!!! ADJUST MANUALY IN CODE !!!!!!!
     #train
-    'number_of_env': 8,
+    'number_of_env': 16,
     'Total_timesteps': int(10e6),
     'train_freq': 1,
     'Reward_Function': '(-self.Rew_distrav_fact*(np.linalg.norm(self.reward_state[0:2]-prev_state[0:2]))-self.Rew_disway_fact*(np.linalg.norm(self.TARGET_POS[0:2]-self.reward_state[0:2])**4)-self.Rew_step_fact*1 +self.Rew_tardis_fact*(prev_tar_dis-self.target_dis))'
 }
 
-#DDPG = Train_DDPG(parameters=parameters,train_gui=False)
-SAC = Train_SAC(parameters=parameters,train_gui=False)
+DDPG = Train_DDPG(parameters=parameters,train_gui=False)
+#SAC = Train_SAC(parameters=parameters,train_gui=False)
 
-#DDPG.train_DDPG()
-SAC.train_SAC()
+DDPG.train_DDPG()
+#SAC.train_SAC()
